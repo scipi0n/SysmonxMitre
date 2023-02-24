5605eb49f1c3712a8cb84c77945f5cc899a3c6c1
scipi0n <90568604+scipi0n@users.noreply.github.com>
2023-02-24 11:47:18 +0100

    Delete myapp directory

--- myapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('', views.home),
-    path('upload_file', views.upload_file, name='upload_file')
-]
